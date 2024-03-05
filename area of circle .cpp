45488b4e9b479d2ce6fe1c9c3ae00dfa292d0373
Tirthkanani <160564970+Tirthkanani@users.noreply.github.com>
2024-03-05 06:25:17 +0530

    Add files via upload

---
+++ area of circle .cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+float area(int, float pi=3.14);
+int main()
+{
+ int r;
+ float ans;
+ cout<<"Enter any value of radius: ";
+ cin>>r;
+ ans = area(r);
+ cout<<"Area of Circle = "<<ans;
+ return 0;
+}
+float area(int r, float pi)
+{
+ float a = pi * r * r;
+ return(a);
+ }
